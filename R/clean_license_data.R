e7dcf2000873d822e1b8333b2fcf94d541bbd894
Carly Lovas <clovas@gmri.org>
2023-12-26 16:21:39 -0500

    updated license networks

--- R/clean_license_data.R
+++ R/clean_license_data.R
@@ -41,13 +41,52 @@ clean_license_data %>%
 #   #relocate(group, .after = license_type) %>% 
 #   relocate(description, .after = license_type) -> license_by_description
 
+
+# Before I create the portfolio, I need to combine license types with multiple codes 
+# making plot labels
+license_types %>% 
+  mutate(description = tolower(description)) %>%
+  group_by(description) %>% 
+  nest() %>%
+  mutate(count = map_dbl(data, function(x){nrow(x)})) -> plot_labels
+
+plot_labels %>%
+  filter(!description %in% c("scallop diver", "scallop dragger")) %>%
+  filter(count == "2") %>%
+  mutate(recent = map(data, function(df){df %>% rowid_to_column() %>% filter(rowid == "2") %>% select(license_type)})) %>% 
+  unnest(recent) %>%
+  select(description, license_type) -> dup_codes
+
+plot_labels %>%
+  select(description) %>% 
+  filter(description == "scallop diver") %>%
+  mutate(license_type = "sdi") %>%
+  full_join(plot_labels %>%
+              select(description) %>% 
+              filter(description == "scallop dragger") %>%
+              mutate(license_type = "sb")) -> scallop_labels 
+
+plot_labels %>% 
+  filter(!count == "2") %>% 
+  unnest(data) %>% 
+  select(description, license_type) %>%
+  full_join(dup_codes) %>%
+  full_join(scallop_labels) %>%
+  arrange(description) %>%
+  drop_na()-> plot_labels
+
+write_csv(plot_labels, file = here("Data", "cleaned_data_labels.csv"))
+
+# created group portfolio
 grouped_license_portfolio <- clean_license_data %>%
   mutate(licensed =1 , 
          row = row_number())%>% 
   ungroup(license_type) %>%
-  select(landings_number,license_year,description,licensed,row)%>% 
-  pivot_wider(names_from=description,values_from=licensed,names_expand=TRUE,values_fill = list(licensed=0))%>%
-  select(-row)%>%
+  select(landings_number,license_year,description,licensed,row)%>%
+  left_join(plot_labels) %>%
+  pivot_wider(names_from=license_type,values_from=licensed,names_expand=TRUE,values_fill = list(licensed=0))%>%
+  select(-row) %>%
+  select(-description) %>%
   group_by(landings_number,license_year)%>%
   summarise(across(everything(),sum))
 
